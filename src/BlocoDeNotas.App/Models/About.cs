d0b6df71753bf2652d8c1e5e763de5715b795170
PauloAlves8039 <pj38alves@gmail.com>
2024-02-06 10:56:36 -0300

    Criando e implementando a classe About

---
+++ src/BlocoDeNotas.App/Models/About.cs
@@ -0,0 +1,19 @@
+﻿namespace BlocoDeNotas.App.Models
+{
+    public class About
+    {
+        public string Title{ get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public string MoreInfoUrl { get; set; }
+
+        public About()
+        {
+            Title = AppInfo.Name;
+            Version = AppInfo.VersionString;
+            Description = "Este app tem como objetivo simular um bloco de notas no qual você ]" +
+                          "poderá anotar tudo o que quiser.";
+            MoreInfoUrl = "https://github.com/PauloAlves8039";
+        }
+    }
+}
